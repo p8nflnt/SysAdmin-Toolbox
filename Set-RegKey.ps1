7505b484aa918637775cdcc425999b5e03dd7c4a
Payton Flint <47405653+p8nflnt@users.noreply.github.com>
2023-08-14 20:38:29 -0500

    Update and rename Modify-RegistryKey.ps1 to Set-RegKey.ps1

---
+++ Set-RegKey.ps1
@@ -0,0 +1,84 @@
+<#
+.SYNOPSIS
+    Set registry key to your desired value & return action taken 
+    (w/ silent option)
+
+.NOTES
+    Name: Set-RegKey
+    Author: Payton Flint
+    Version: 1.1
+    DateCreated: 2023-Aug
+
+.LINKS
+    https://paytonflint.com/powershell-add-modify-registry-key/
+#>
+
+
+$regPath  = ‘<INSERT PATH>'
+$regName  = ‘<INSERT NAME>'
+$regValue = ‘<INSERT VALUE>'
+
+Function Set-RegKey {
+    param(
+    $regPath,
+    $regName,
+    $regValue,
+    [bool]$silent
+    )
+    $regFull = Join-Path $regPath $regName
+        Try {
+                $CurrentKeyValue = (Get-ItemProperty -Path $regPath -Name $regName -ErrorAction SilentlyContinue).$regName
+                If (Test-Path $regPath) {
+                    If ($CurrentKeyValue -eq $regValue) {
+                        If (!($silent)) {
+                            Write-Host -ForegroundColor Green 'Registry key' $regFull 'value is set to the desired value of' $regValue'.'
+                        }
+                        $script:regTest = $True  
+                    } Else {
+                        If (!($silent)) {
+                            Write-Host -ForegroundColor Red 'Registry key' $regFull 'value is not' $regValue'.'
+                            Write-Host -ForegroundColor Cyan 'Setting registry key' $regFull 'value to' $regValue'.'
+                        }
+                        New-ItemProperty -Path $regPath -Name $regName -Value $regValue -PropertyType DWORD -Force | Out-Null
+                        $CurrentKeyValue = (Get-ItemProperty -Path $regPath -Name $regName -ErrorAction SilentlyContinue).$regName
+                        If ($CurrentKeyValue -eq $regValue) {
+                            If (!($silent)) {
+                                Write-Host -ForegroundColor Green 'Registry key' $regFull 'value is set to the desired value of' $regValue'.'
+                            }
+                            $script:regTest = $True  
+                        } Else {
+                            If (!($silent)) {
+                                Write-Host -ForegroundColor Red 'Registry key' $regFull 'value could not be set to' $regValue '.'
+                            }
+                        }
+                    }
+                } Else {
+                    If (!($silent)) {
+                        Write-Host -ForegroundColor Red 'Registry key' $regFull 'path does not exist.'
+                        Write-Host -ForegroundColor Cyan 'Creating registry key' $regFull'.'
+                    }
+                    New-Item -Path $regPath -Force | Out-Null
+                    If (!($silent)) {
+                        Write-Host -ForegroundColor Cyan 'Setting registry key' $regFull 'value to' $regValue'.'
+                    }
+                    New-ItemProperty -Path $regPath -Name $regName -Value $regValue -PropertyType DWORD -Force | Out-Null
+                    $CurrentKeyValue = (Get-ItemProperty -Path $regPath -Name $regName -ErrorAction SilentlyContinue).$regName
+                    If ($CurrentKeyValue -eq $regValue) {
+                        If (!($silent)) {
+                            Write-Host -ForegroundColor Green 'Registry key' $regFull 'value is set to the desired value of' $regValue'.'
+                        }
+                        $script:regTest = $True  
+                    } Else {
+                        If (!($silent)) {
+                            Write-Host -ForegroundColor Red 'Registry key' $regFull 'value could not be set to' $regValue '.'
+                        }
+                    }
+                }
+        } Catch {
+            If (!($silent)) {
+                Write-Host -ForegroundColor Red 'Registry key' $regFull 'value could not be set to' $regValue '.'
+            }
+        }
+} # End Function Set-RegKey
+
+Set-RegKey -regPath $regPath -regName $regName -regValue $regValue -silent <BOOL>
