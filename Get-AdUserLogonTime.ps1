07732db160dde225f0234bb502e4258e9d474c9a
Payton Flint <47405653+p8nflnt@users.noreply.github.com>
2023-09-13 19:29:10 -0500

    Create Get-AdUserLogonTime.ps1

---
+++ Get-AdUserLogonTime.ps1
@@ -0,0 +1,37 @@
+<#
+.SYNOPSIS
+    Retrieve last logon time information for an AD user.
+
+.NOTES
+    Name: Get-AdUserLogonTime.ps1
+    Author: Payton Flint
+    Version: 1.0
+    DateCreated: 2023-Sep
+
+.LINK
+    https://github.com/p8nflnt/
+#>
+
+Function Get-AdUserLogonTime {
+    param (
+        $name
+    )
+
+    $info = Get-ADUser -Properties lastLogon, lastLogonTimestamp -Filter "Name -eq '$name'" `
+    | Select-Object  name, userPrincipalName, lastLogon, lastLogonTimestamp
+
+    $userPrincipalName  = $info.userPrincipalName
+    $lastLogon          = [datetime]::FromFileTime($info.lastLogon)
+    $lastLogonTimestamp = [datetime]::FromFileTime($info.lastLogonTimestamp)
+
+    $userObj = [PSCustomObject]@{
+        Name               = "$name"
+        userPrincipalName  = "$userPrincipalName"
+        lastLogon          = "$lastLogon"
+        lastLogonTimestamp = "$lastLogonTimestamp"
+    }
+
+    return $userObj
+}
+
+$result = Get-AdUserLogonTime -name '<INSERT USER NAME>'
